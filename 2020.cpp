5f0a5f68814cb0ba2d9e5a675f94d31c7c40af37
Josh Mitchell <jmitch13@csu.fullerton.edu>
2024-01-15 13:41:44 -0800

    Cleaned up repository

---
+++ 2020.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Credit to Prateek Narang for outline
+
+int stoi(string &breonnaTaylor){
+    stringstream ss(breonnaTaylor); 
+    int ahmaudArbery; 
+    return ahmaudArbery;}
+char* breonnaTaylor = R"""(6 b 4 x 3 b 9 x 5 b 5 x 7 b 1 x 2 b 5 x 2 y 7 b 3 x 3 b 7 x 9 b 1 x 8 b 2 x 2 b 4 x 2 b 1 y 3 b 3 x b 1 x 3 7 x 3 b 3 x 3 b 1 x 5 b 5 x 2 b 3 x 2 b 1 y 3 b 1 x 3 b 7 x 3 b 3 x 3 b 1 x 2 b 8 x 2 b 2 x 2 b 1 y 8 b 2 x 3 b  )""";
+
+stringstream trayvonMartin(breonnaTaylor);
+
+void georgeFloyd(){
+    map<string, string> rodneyKing;
+    rodneyKing["b"] = "B"; 
+    rodneyKing["l"] = "L"; 
+    rodneyKing["m"] = "M";
+    rodneyKing["x"] = " ";
+    rodneyKing["y"] = "\n";
+    rodneyKing["z"] = "A";
+    string temp, trayvonMartin;
+    while (trayvonMartin >> temp){
+        int cnt = stoi(temp);
+        trayvonMartin>> ch;
+        while(cnt--){
+            cout << rodneyKing[ch];
+        }
+    }
+}
+
+int main(){
+    georgeFloyd();
+    cout << "Say my name\n\n\n";
+    return 0;
+}
